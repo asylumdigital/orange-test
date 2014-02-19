7227c7a42b5ce75136363567a93133eb9c58a8bf
root <asylumdigital>
2014-02-19 07:14:05 +0000

    Adding test files

--- docroot/assets/js/global.js
+++ docroot/assets/js/global.js
@@ -1,6 +1,28 @@
 var orange = {
 	init: function(){
+		orange.layout.init();
+	},
+	layout: {
+		init: function(){
+			this.carousel();
+			this.touchNav();
+		},
+		carousel: function(){
+			$('#features').owlCarousel({
+				navigation : false, // Show next and prev buttons
+				slideSpeed : 300,
+				paginationSpeed : 400,
+				singleItem:true
+			});
+		},
+		touchNav: function(){
+			$('#menu-toggle').click(function(e){
+				e.preventDefault();
+				$('body').toggleClass('js-nav');
 
+
+			});
+		}
 	}
 }
 
